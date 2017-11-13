09234e78a2ec2f2744d92eb2a13ee97248f901a9
dannypsnl <dannypsnl@gmail.com>
2017-11-13 13:31:08 +0800

    A Lot helper method of Lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -10,6 +10,8 @@ import (
 type ItemType int
 type Pos int
 
+const EOF = -1
+
 const (
 	ItemError ItemType = iota
 
@@ -87,12 +89,54 @@ func (lex *Lexer) run() {
 	close(lex.items)
 }
 
+// next returns the next rune in the input.
+func (l *Lexer) next() rune {
+	if int(l.pos) >= len(l.input) {
+		l.width = 0
+		return EOF
+	}
+	r, w := utf8.DecodeRuneInString(l.input[l.pos:])
+	l.width = Pos(w)
+	l.pos += l.width
+	return r
+}
+
+// peek returns but does not consume the next rune in the input.
+func (l *Lexer) peek() rune {
+	r := l.next()
+	l.backup()
+	return r
+}
+
+// backup steps back one rune. Can only be called once per call of next.
+func (l *Lexer) backup() {
+	l.pos -= l.width
+}
+
+// emit passes an Item back to the client.
 func (l *Lexer) emit(t ItemType) {
 	// Item {ItemType, Pos, Val}
 	l.items <- Item{t, l.start, l.input[l.start:l.pos]}
 	l.start = l.pos
 }
 
+// ignore drop not yet emitted parts
+func (l *Lexer) ignore() {
+	l.start = l.pos
+}
+
+// stateFn is a function need get info from Lexer and return next stateFn
 type stateFn func(*Lexer) stateFn
 
-func lexWhiteSpace(l *Lexer) stateFn {}
+func lexWhiteSpace(l *Lexer) stateFn {
+	for {
+		if l.next() == EOF {
+			break
+		}
+	}
+	if l.pos > l.start {
+		l.emit(ItemError)
+	}
+	l.emit(ItemEOF)
+	return nil
+}
