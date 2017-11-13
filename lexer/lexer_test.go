4c899d888ef8d7cbee4d9fd9705696ba768cfb38
dannypsnl <dannypsnl@gmail.com>
2017-11-13 14:09:10 +0800

    Improve test case

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,14 +7,20 @@ import (
 
 func TestLexer(t *testing.T) {
 	var results []Item
-	lexer := Lex("lex", "123")
+	lexer := Lex("lex", "123 name")
 	for item := lexer.NextItem(); item.Type != ItemEOF; item = lexer.NextItem() {
 		results = append(results, item)
 	}
 	if strings.Compare(lexer.name, "lex") != 0 {
 		t.Error("Lexer name is wrong!")
 	}
-	if results[0].Type != ItemInt {
-		t.Error("First token should be a Int Literal!")
+	expected := []ItemType{
+		ItemInt,
+		ItemIdent,
+	}
+	for i, result := range results {
+		if result.Type != expected[i] {
+			t.Errorf("Expected: %q, Actual: %q", expected[i], result.Type)
+		}
 	}
 }
