0c412c9c739743d5331f6cac434bc7573150e8df
dannypsnl <dannypsnl@gmail.com>
2017-11-15 17:51:37 +0800

    write some comment and next code.

--- parser/parser.go
+++ parser/parser.go
@@ -27,8 +27,13 @@ func Parse(filename, source_code string) []Ast {
 func startParse(l *lexer.Lexer, tree []Ast, lookingFor rune) {
 	for token := l.NextItem(); token.Type != lexer.ItemEOF; {
 		switch t := token.Type; t {
+		// First, we handle keyword[let], it's a statement call DefineVar, so we will get a Ast[DefineVar].
 		case lexer.ItemKwLet:
 			tree = append(tree, parseDefineVar(l))
+		// If meet keyword[fn], it's a statement call DefineFn, so we will get a Ast[DefineFn]
+		case lexer.ItemKwFn:
+			tree = append(tree, parseDefineFn(l))
+		// If we meet a error item, that mean we didn't receive this token, it could be a bug that we didn't consider. Please report.
 		case lexer.ItemError:
 			fmt.Println(token.Val)
 		default:
