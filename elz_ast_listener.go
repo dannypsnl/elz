81b85a765ead9fe7d6648e368fb56d91df7f524d
dannypsnl <dannypsnl@gmail.com>
2017-11-21 12:52:40 +0800

    Deal with rule

--- elz_ast_listener.go
+++ elz_ast_listener.go
@@ -8,12 +8,21 @@ import (
 
 type ElzListener struct {
 	*parser.BaseElzListener
+	exportThis bool
 }
 
 func NewElzListener() *ElzListener {
-	return new(ElzListener)
+	return &ElzListener{
+		exportThis: false,
+	}
 }
 
 func (s *ElzListener) EnterProg(ctx *parser.ProgContext) {
 	fmt.Println(`Elz prog`)
+}
+func (s *ElzListener) EnterExportor(*parser.ExportorContext) {
+	s.exportThis = true
+}
+func (s *ElzListener) EnterVarDefine(ctx *parser.VarDefineContext) {
+	fmt.Println("mmutable:", ctx.GetMut())
 }
