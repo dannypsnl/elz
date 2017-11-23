ed30f67c01542ac0fcad8678eccc2b8adae63d5f
dannypsnl <dannypsnl@gmail.com>
2017-11-23 13:29:14 +0800

    make sure how to use double

--- ast/llvmir/main.go
+++ ast/llvmir/main.go
@@ -10,12 +10,12 @@ import (
 func main() {
 	m := ir.NewModule()
 
-	f32 := types.Float
+	f64 := types.Double
 	i32 := types.I32
 	main := m.NewFunction("main", i32)
 	entry := main.NewBlock("entry")
 	entry.NewFMul(
-		constant.NewFloatFromString("4.3300000e+00", f32), constant.NewFloatFromString("2.10000000e+00", f32),
+		constant.NewFloatFromString("4.33", f64), constant.NewFloatFromString("2.1", f64),
 	)
 	entry.NewRet(constant.NewInt(0, i32))
 
