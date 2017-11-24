b73cdd4e31ad84caa8108783cbbe7d58ea8f5348
dannypsnl <dannypsnl@gmail.com>
2017-11-24 16:53:19 +0800

    Use pragma once make sure won't redefine, and deal a rule

--- elzc/src/AstBuilder.h
+++ elzc/src/AstBuilder.h
@@ -1,5 +1,10 @@
+#pragma once
+
 #include "ElzBaseListener.h" // parser/ElzBaseListener.h
+#include "ElzParser.h"       // parser/ElzParser.h
 
 namespace elz {
-class AstBuilder : public ElzBaseListener {};
+class AstBuilder : public ElzBaseListener {
+  void enterVarDefine(ElzParser::DefineContext *ctx) {}
+};
 } // namespace elz
