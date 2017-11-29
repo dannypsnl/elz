9e91ec6dc18a2d48a96a9650c29fd473e55fc70c
dannypsnl <dannypsnl@gmail.com>
2017-11-29 09:27:25 +0800

    expr ast header, llvm build seems fail, I have to rebuild again

--- elzc/src/AstBuilder.h
+++ elzc/src/AstBuilder.h
@@ -3,6 +3,8 @@
 #include "ElzBaseListener.h" // parser/ElzBaseListener.h
 #include "ElzParser.h"       // parser/ElzParser.h
 
+#include "./ast/expr.h"
+
 namespace elz {
 class AstBuilder : public ElzBaseListener {
   void enterVarDefine(ElzParser::DefineContext *ctx) {}
--- elzc/src/ast/expr.h
+++ elzc/src/ast/expr.h
@@ -1,3 +1,44 @@
+#pragma once
+
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Value.h"
+
+#include <map>
+#include <memory>
+#include <string>
+
 namespace ast {
-class Expr {};
+
+static LLVMContext TheContext;
+static IRBuilder<> Builder(TheContext);
+static std::unique_ptr<Module> TheModule;
+static std::map<std::string, Value *> NamedValues;
+
+class Expr {
+public:
+  virtual ~Expr() {}
+  virtual Value *codegen() = 0;
+};
+
+class Number : public Expr {
+  double Val;
+
+public:
+  // Maybe should use string val and transport inside?
+  Number(double val) : Val{val} {}
+  virtual Value *codegen();
+};
+
+class Variable : public Expr {
+  std::string Val;
+
+public:
+  Variable(std::string val) : Val{val} {}
+  virtual Value *codegen();
+};
+
+class BinaryExpr : public Expr {};
+
 } // namespace ast
