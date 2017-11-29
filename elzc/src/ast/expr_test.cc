a3b5fb1f1a8606b5a7549f3b8073538d2cbe738f
dannypsnl <dannypsnl@gmail.com>
2017-11-29 16:27:14 +0800

    print module, make sure it's work

--- elzc/src/ast/expr_test.cc
+++ elzc/src/ast/expr_test.cc
@@ -1,7 +1,8 @@
 #include "./expr.h"
+#include "llvm/Support/raw_ostream.h"
 #include "gtest/gtest.h"
 
 TEST(CodegenOfAST, Number) {
   ast::Number num{10};
-  // ast::TheModule->dump();
+  ast::TheModule->print(llvm::errs(), nullptr);
 }
