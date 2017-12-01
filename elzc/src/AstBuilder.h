0cd6184d2f58c347110495aab344975af0ea4216
dannypsnl <dannypsnl@gmail.com>
2017-12-01 14:22:40 +0800

    impl define rule, a little part

--- elzc/src/AstBuilder.h
+++ elzc/src/AstBuilder.h
@@ -4,11 +4,16 @@
 #include "ElzParser.h"       // parser/ElzParser.h
 
 #include "./ast/expr.h"
+#include "./ast/top_statement.h"
+#include <iostream>
 
 namespace elz {
 class AstBuilder : public ElzBaseListener {
 public:
   // TODO: Put context at here, and improve it.
-  void enterVarDefine(ElzParser::DefineContext *ctx) { ; }
+  void enterDefine(ElzParser::DefineContext *ctx) override {
+    ast::VariableDefine new_var{ctx->ID()->getText()};
+    std::cout << "new var: " << ctx->ID()->getText() << std::endl;
+  }
 };
 } // namespace elz
