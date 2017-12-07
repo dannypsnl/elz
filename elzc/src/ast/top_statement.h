a6094f1ae3d50c0e6018de87e9e289a36b98c14a
dannypsnl <dannypsnl@gmail.com>
2017-12-07 12:02:10 +0800

    seems not only go-bindings problem, is defination of llvm-ir

--- elzc/src/ast/top_statement.h
+++ elzc/src/ast/top_statement.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "llvm/ADT/APFloat.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/IR/GlobalValue.h"
 #include "llvm/IR/GlobalVariable.h"
 
 #include <string>
 
+using llvm::ConstantFP;
 using llvm::GlobalValue;
 using llvm::GlobalVariable;
 
@@ -19,6 +22,8 @@ public:
   void codegen() {
     TheModule->getOrInsertGlobal(name, Builder.getInt32Ty());
     GlobalVariable *gvar = TheModule->getNamedGlobal(name);
+    gvar->setInitializer(
+        ConstantFP::get(TheModule->getContext(), llvm::APFloat(3.1415926)));
     gvar->setLinkage(GlobalValue::CommonLinkage);
   }
 };
