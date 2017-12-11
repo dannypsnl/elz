1eab29fc832ce0036ee0c05168455a38e4c22333
dannypsnl <dannypsnl@gmail.com>
2017-12-11 14:39:31 +0800

    pratice go-llvm

---
+++ ast/go-llvm/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"llvm.org/llvm/bindings/go/llvm"
+)
+
+type Context struct {
+	m       llvm.Module
+	ctx     llvm.Context
+	builder llvm.Builder
+}
+
+func main() {
+	ctx := &Context{
+		m:       llvm.NewModule("main"),
+		ctx:     llvm.NewContext(),
+		builder: llvm.NewBuilder(),
+	}
+
+	// How to create string
+	aStr := llvm.ConstString("\\a你好, llvm, $@#%^!&!)~!#*(@#+_)(*&GBJNLSfdlbc)", false)
+	gVal := llvm.AddGlobal(ctx.m, aStr.Type(), "string")
+	gVal.SetInitializer(aStr)
+
+	// How to create a function
+	ft := llvm.FunctionType(aStr.Type(), []llvm.Type{aStr.Type()}, false)
+	llvm.AddFunction(ctx.m, "foo_string_string", ft)
+
+	ft = llvm.FunctionType(ctx.ctx.FloatType(), []llvm.Type{}, false)
+	llvm.AddFunction(ctx.m, "foo_float", ft)
+
+	// How to create a user define type
+	structTp := llvm.StructType([]llvm.Type{
+		llvm.FloatType(),
+	}, false)
+	insStruct := llvm.ConstNamedStruct(structTp, []llvm.Value{llvm.ConstFloat(llvm.FloatType(), 3.14)})
+	gStruct := llvm.AddGlobal(ctx.m, structTp, "A struct")
+	gStruct.SetInitializer(insStruct)
+
+	fmt.Println(ctx.m)
+}
