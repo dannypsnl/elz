34a2e1c675f6b72b4ec0039636e0c102540636c3
dannypsnl <dannypsnl@gmail.com>
2017-12-13 13:49:23 +0800

    test number's type

--- ast/expr_test.go
+++ ast/expr_test.go
@@ -17,7 +17,8 @@ func TestNumber(t *testing.T) {
 		Val: "3.1415926",
 	}
 	result := num.Codegen(ctx)
-	if !result.IsConstant() {
+	if !result.IsConstant() ||
+		result.Type().String() != "FloatType" {
 		result.Dump()
 		t.Error(`error`)
 	}
@@ -54,7 +55,7 @@ func TestBinaryExpr(t *testing.T) {
 	}
 }
 
-func TestBinaryUnsupport(t *testing.T) {
+func TestBinaryForUnsupportOp(t *testing.T) {
 	defer func() {
 		if p := recover(); p == nil {
 			t.Error(`Didn't panic`)
@@ -69,4 +70,17 @@ func TestBinaryUnsupport(t *testing.T) {
 		Op:     "~",
 	}
 	be.Codegen(ctx)
+}
+
+func TestString(t *testing.T) {
+	str := &Str{
+		Val: `"a string with 中文"`,
+	}
+	result := str.Codegen(ctx)
+	if !result.IsConstant() ||
+		// string is [i8] in LLVM IR
+		result.Type().String() != "ArrayType(IntegerType(8 bits)[22])" {
+		result.Dump()
+		t.Error(`error`)
+	}
 }
