8189e3df046de954c794ab96e990c0ddad30a040
dannypsnl <dannypsnl@gmail.com>
2017-12-29 10:17:42 +0800

    create struct[Id], then found impl's missed part, so prepare to rewrite some part like Vars Mapping's type, and Type() should be Type(*ast.Context)

--- ast/idStrNum_expr.go
+++ ast/idStrNum_expr.go
@@ -4,6 +4,21 @@ import (
 	"llvm.org/llvm/bindings/go/llvm"
 )
 
+type Id struct {
+	Val string
+}
+
+func (i *Id) Codegen(ctx *Context) llvm.Value {
+	return ctx.Vars[i.Val]
+}
+
+// At here we can see, ident's type need to logging in Context
+// So Context should send into Type method and Context::Vars
+// need a new structure for usage
+func (i *Id) Type() string {
+	return ""
+}
+
 type Str struct {
 	Val string
 }
