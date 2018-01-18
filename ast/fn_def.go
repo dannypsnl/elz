a8b80828158315329b8e35918f96e7ccb61d118e
dannypsnl <dannypsnl@gmail.com>
2018-01-18 11:35:41 +0800

    [move]fnDef => fn_def.go

---
+++ ast/fn_def.go
@@ -0,0 +1,36 @@
+package ast
+
+import (
+	"llvm.org/llvm/bindings/go/llvm"
+)
+
+type Param struct {
+	Name string
+	Type string
+}
+
+type FnDef struct {
+	Export  bool
+	Name    string
+	Params  []*Param
+	Body    StatList
+	RetType string
+}
+
+func (f *FnDef) Codegen(ctx *Context) llvm.Value {
+	var paramsT []llvm.Type
+	for _, v := range f.Params {
+		paramsT = append(paramsT, convertToLLVMType(v.Type))
+	}
+	retT := convertToLLVMType(f.RetType)
+	ft := llvm.FunctionType(retT, paramsT, false)
+	fn := llvm.AddFunction(ctx.Module, f.Name, ft)
+	entryPoint := llvm.AddBasicBlock(fn, "entry")
+	ctx.Builder.SetInsertPointAtEnd(entryPoint)
+	// TODO: for _, stat := range f.Body {
+	//ctx.Builder.Insert(stat.Codegen(ctx))
+	//}
+	ctx.Builder.CreateRet(llvm.ConstFloat(llvm.FloatType(), 3.14))
+	ctx.Builder.ClearInsertionPoint()
+	return fn
+}
