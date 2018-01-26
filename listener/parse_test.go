1aa4b4c8026b36c602abca9fbfe8a301cd36a0a8
dannypsnl <dannypsnl@gmail.com>
2018-01-26 14:33:43 +0800

    [fix]wrong testing

--- listener/parse_test.go
+++ listener/parse_test.go
@@ -1,7 +1,6 @@
 package listener
 
 import (
-	"strings"
 	"testing"
 )
 
@@ -9,13 +8,12 @@ func TestParse(t *testing.T) {
 	res := NewParse(`
 	let a = 10
 	`)
-	expected := `
-; ModuleID = 'main'
+	expected := `; ModuleID = 'main'
 source_filename = "main"
 
 @a = global float 1.000000e+01
 `
-	if strings.Contains(res, expected) {
+	if res != expected {
 		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
 	}
 }
