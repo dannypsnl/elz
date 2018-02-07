ef046da390d6e6dd1f18f4313075c28814f563fc
routedan <dannypsnl@gmail.com>
2018-02-07 23:40:03 +0800

    [clean]indepen fn rule

---
+++ listener/fn_rule.go
@@ -0,0 +1,30 @@
+package listener
+
+import (
+	"fmt"
+
+	"github.com/elz-lang/elz/parser"
+)
+
+func (s *ElzListener) EnterFnDefine(ctx *parser.FnDefineContext) {
+	// TODO: complete fn generate
+	s.fnBuilder = NewFnBuilder().
+		Name(ctx.ID().GetText()).
+		Export(s.exportThis)
+}
+
+func (s *ElzListener) EnterParam(ctx *parser.ParamContext) {
+	// TODO: get type info
+	fmt.Println(ctx.ID().GetText())
+	// insert ID.Text into fnBuilder
+	// set flag let fnBuilder record the type when pass type rule
+	// clean flag
+}
+
+func (s *ElzListener) ExitFnDefine(ctx *parser.FnDefineContext) {
+	s.AstList = append(s.AstList,
+		s.fnBuilder.generate(),
+	)
+	// TODO: fn need builder to create at here, because it will cross several rules
+	s.fnBuilder = nil
+}
--- listener/listener.go
+++ listener/listener.go
@@ -99,26 +99,3 @@ func (s *ElzListener) ExitDefine(ctx *parser.DefineContext) {
 	// Record type for compiler
 	s.context.VarsType[name] = typ
 }
-
-func (s *ElzListener) EnterFnDefine(ctx *parser.FnDefineContext) {
-	// TODO: complete fn generate
-	s.fnBuilder = NewFnBuilder().
-		Name(ctx.ID().GetText()).
-		Export(s.exportThis)
-}
-
-func (s *ElzListener) EnterParam(ctx *parser.ParamContext) {
-	// TODO: get type info
-	fmt.Println(ctx.ID().GetText())
-	// insert ID.Text into fnBuilder
-	// set flag let fnBuilder record the type when pass type rule
-	// clean flag
-}
-
-func (s *ElzListener) ExitFnDefine(ctx *parser.FnDefineContext) {
-	s.AstList = append(s.AstList,
-		s.fnBuilder.generate(),
-	)
-	// TODO: fn need builder to create at here, because it will cross several rules
-	s.fnBuilder = nil
-}
