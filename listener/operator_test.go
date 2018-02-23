5301a49b6780eb955534d4f6dc6af1a2bd4caeb4
routedan <dannypsnl@gmail.com>
2018-02-23 23:59:08 +0800

    clean: cleanup test

--- listener/operator_test.go
+++ listener/operator_test.go
@@ -10,13 +10,14 @@ func TestNumStrExpression(t *testing.T) {
 	let a = 3 * 2
 	let mut b = "abcde"
 	`)
+
 	expected := `; ModuleID = 'main'
 source_filename = "main"
 
 @a = global float 6.000000e+00
 @b = global [7 x i8] c"\22abcde\22"
 `
-	//if !strings.Contains(res, expected) {
+
 	if res != expected {
 		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
 	}
--- listener/parse_test.go
+++ listener/parse_test.go
@@ -8,11 +8,13 @@ func TestParse(t *testing.T) {
 	res := NewParse(`
 	let a = 10
 	`)
+
 	expected := `; ModuleID = 'main'
 source_filename = "main"
 
 @a = global float 1.000000e+01
 `
+
 	if res != expected {
 		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
 	}
