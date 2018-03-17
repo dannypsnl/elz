4941109c36bb221903273c51e1fec6443a887e6b
routedan <dannypsnl@gmail.com>
2018-03-17 22:43:38 +0800

    test: find out we can just use parameter in go-llvm very easy
    problem: Don't know how to use API
    reason: Most example are C++ version, and other are old example
    solution: Try it
    project: elz
    note: N/A

--- ast/go-llvm/main.go
+++ ast/go-llvm/main.go
@@ -18,14 +18,6 @@ func main() {
 		builder: llvm.NewBuilder(),
 	}
 
-	// main function
-	ft := llvm.FunctionType(ctx.ctx.Int32Type(), []llvm.Type{}, false)
-	mainFn := llvm.AddFunction(ctx.m, "main", ft)
-	mainBlock := llvm.AddBasicBlock(mainFn, "entry")
-	ctx.builder.SetInsertPointAtEnd(mainBlock)
-	ctx.builder.CreateRet(llvm.ConstInt(llvm.Int32Type(), 0, false))
-	ctx.builder.ClearInsertionPoint()
-
 	// How to create global const float
 	aNum := llvm.ConstFloatFromString(llvm.FloatType(), `3.1415926`)
 	bNum := llvm.ConstFloatFromString(llvm.FloatType(), `2.5`)
@@ -43,7 +35,7 @@ func main() {
 
 	// How to create a function
 	// llvm.FunctionType( retrun type, params types, is var arg: bool )
-	ft = llvm.FunctionType(aStr.Type(), []llvm.Type{aStr.Type()}, false)
+	ft := llvm.FunctionType(aStr.Type(), []llvm.Type{aStr.Type()}, false)
 	llvm.AddFunction(ctx.m, "main::foo_string_string", ft)
 
 	ft = llvm.FunctionType(llvm.Int32Type(), []llvm.Type{
@@ -51,20 +43,31 @@ func main() {
 		llvm.Int32Type(),
 	}, false)
 	addFn := llvm.AddFunction(ctx.m, "main::add", ft)
-	addBlock := llvm.AddBasicBlock(addFn, "entry")
-	ctx.builder.SetInsertPointAtEnd(addBlock)
-	ctx.builder.CreateRet(llvm.ConstInt(llvm.Int32Type(), 0, false))
-	ctx.builder.ClearInsertionPoint()
-	fmt.Println(addFn.ParamsCount())
 	args := []string{"lv", "rv"}
 	i := 0
 	for _, param := range addFn.Params() {
 		param.SetName(args[i])
 		i++
 	}
+	addBlock := llvm.AddBasicBlock(addFn, "entry")
+	ctx.builder.SetInsertPointAtEnd(addBlock)
+	returnVal := ctx.builder.CreateAdd(addFn.Param(0), addFn.Param(1), "result")
+	ctx.builder.CreateRet(
+		returnVal,
+	)
+	ctx.builder.ClearInsertionPoint()
 
-	ft = llvm.FunctionType(ctx.ctx.FloatType(), []llvm.Type{}, false)
-	llvm.AddFunction(ctx.m, "main::foo_float", ft)
+	// main function
+	ft = llvm.FunctionType(ctx.ctx.Int32Type(), []llvm.Type{}, false)
+	mainFn := llvm.AddFunction(ctx.m, "main", ft)
+	mainBlock := llvm.AddBasicBlock(mainFn, "entry")
+	ctx.builder.SetInsertPointAtEnd(mainBlock)
+	ctx.builder.CreateCall(addFn, []llvm.Value{
+		llvm.ConstInt(llvm.Int32Type(), 2, false),
+		llvm.ConstInt(llvm.Int32Type(), 3, false),
+	}, "tmp")
+	ctx.builder.CreateRet(llvm.ConstInt(llvm.Int32Type(), 0, false))
+	ctx.builder.ClearInsertionPoint()
 
 	// How to create a user define type
 	structTp := llvm.StructType([]llvm.Type{
