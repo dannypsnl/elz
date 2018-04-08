edc2d640c40f7b2961af5c354d13c868e0355697
dannypsnl <dannypsnl@gmail.com>
2018-04-08 21:21:08 +0800

    test: const computing in function

--- listener/fn_test.go
+++ listener/fn_test.go
@@ -4,7 +4,28 @@ import (
 	"testing"
 )
 
-func TestNumAddFunction(t *testing.T) {
+func TestConstComputingInFunction(t *testing.T) {
+	res := NewParse(`
+	fn foo() {
+		let a: i32 = 1 + 3
+	}
+	`)
+
+	expected := `; ModuleID = 'main'
+source_filename = "main"
+
+define void @foo() {
+entry:
+  %a = alloca i32
+  store i32 4, i32* %a
+}
+`
+	if res != expected {
+		t.Errorf("Function Error; expected: `%s`\nactual: `%s`", expected, res)
+	}
+}
+
+func TestOverloadingFunction(t *testing.T) {
 	res := NewParse(`
 	fn add(l: num, r: num) -> num {}
 	fn add(l: i32, r: i32) -> i32 {}
@@ -21,7 +42,6 @@ define i32 @add.1(i32 %l, i32 %r) {
 entry:
 }
 `
-
 	if res != expected {
 		t.Errorf("Function Error; expected: `%s`\nactual: `%s`", expected, res)
 	}
@@ -40,7 +60,6 @@ entry:
   ret i32 0
 }
 `
-
 	if res != expected {
 		t.Errorf("Main Function Error; expected: `%s`\nactual: `%s`", expected, res)
 	}
