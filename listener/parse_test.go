fcf847c41f6bac841b3fed9a3f23fb6bac611d4b
dannypsnl <dannypsnl@gmail.com>
2018-04-12 09:52:50 +0800

    test: use Module VerifyModule testing result
    note: I don't know how many test should use this, or add as last check after generated the module

--- listener/parse_test.go
+++ listener/parse_test.go
@@ -2,6 +2,11 @@ package listener
 
 import (
 	"testing"
+
+	"github.com/elz-lang/elz/parser"
+
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"llvm.org/llvm/bindings/go/llvm"
 )
 
 func TestParse(t *testing.T) {
@@ -17,5 +22,20 @@ source_filename = "main"
 
 	if res != expected {
 		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
+	}
+}
+
+func TestVerifyModule(t *testing.T) {
+	input := antlr.NewInputStream(`a = 10`)
+	lexer := parser.NewElzLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, 0)
+	p := parser.NewElzParser(stream)
+	p.BuildParseTrees = true
+	tree := p.Prog()
+	listener := /*listener.*/ New()
+	antlr.ParseTreeWalkerDefault.Walk(listener, tree)
+
+	if err := llvm.VerifyModule(listener.Module(), llvm.PrintMessageAction); err != nil {
+		t.Error("VerifyModule fail")
 	}
 }
