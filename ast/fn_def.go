b44417161578fcdf28bc9e4af331ee425a2bb356
dannypsnl <dannypsnl@gmail.com>
2018-04-16 00:04:19 +0800

    refactor: replace function

--- ast/fn_def.go
+++ ast/fn_def.go
@@ -17,6 +17,36 @@ type FnDef struct {
 	RetType string
 }
 
+func (f *FnDef) Codegen(ctx *Context) llvm.Value {
+	f.setupMissParamType()
+
+	fn := llvm.AddFunction(ctx.Module, f.Name,
+		llvm.FunctionType(f.returnType(), f.paramsType(), false),
+	)
+
+	for i, param := range fn.Params() {
+		param.SetName(f.Params[i].Name)
+	}
+
+	entryPoint := llvm.AddBasicBlock(fn, "entry")
+
+	ctx.Builder.SetInsertPointAtEnd(entryPoint)
+	for _, stat := range f.Body {
+		stat.Codegen(ctx)
+	}
+	ctx.Builder.ClearInsertionPoint()
+	if f.Name == "main" {
+		generateMainFn(ctx.Builder, entryPoint)
+	}
+	return fn
+}
+
+func generateMainFn(builder llvm.Builder, entryPoint llvm.BasicBlock) {
+	builder.SetInsertPointAtEnd(entryPoint)
+	builder.CreateRet(llvm.ConstInt(llvm.Int32Type(), 0, false))
+	builder.ClearInsertionPoint()
+}
+
 func (f *FnDef) setupMissParamType() {
 	var cache string
 	for i := len(f.Params); i > 0; i-- {
@@ -47,34 +77,4 @@ func (f *FnDef) paramsType() []llvm.Type {
 		paramsT = append(paramsT, convertToLLVMType(v.Type))
 	}
 	return paramsT
-}
-
-func (f *FnDef) Codegen(ctx *Context) llvm.Value {
-	f.setupMissParamType()
-
-	fn := llvm.AddFunction(ctx.Module, f.Name,
-		llvm.FunctionType(f.returnType(), f.paramsType(), false),
-	)
-
-	for i, param := range fn.Params() {
-		param.SetName(f.Params[i].Name)
-	}
-
-	entryPoint := llvm.AddBasicBlock(fn, "entry")
-
-	ctx.Builder.SetInsertPointAtEnd(entryPoint)
-	for _, stat := range f.Body {
-		stat.Codegen(ctx)
-	}
-	ctx.Builder.ClearInsertionPoint()
-	if f.Name == "main" {
-		generateMainFn(ctx.Builder, entryPoint)
-	}
-	return fn
-}
-
-func generateMainFn(builder llvm.Builder, entryPoint llvm.BasicBlock) {
-	builder.SetInsertPointAtEnd(entryPoint)
-	builder.CreateRet(llvm.ConstInt(llvm.Int32Type(), 0, false))
-	builder.ClearInsertionPoint()
 }
