c7794bedc3d0242cbe6888a7de4012fa35905152
dannypsnl <dannypsnl@gmail.com>
2018-04-19 16:36:46 +0800

    test: test HasNoError

--- errors/error_reporter_test.go
+++ errors/error_reporter_test.go
@@ -12,3 +12,14 @@ func TestEmit(t *testing.T) {
 		t.Error("Didn't emit success")
 	}
 }
+
+func TestHasNoError(t *testing.T) {
+	reporter := NewReporter()
+	if !reporter.HasNoError() {
+		t.Error(`Initial state of Reporter is wrong, should not contain any error`)
+	}
+	reporter.Emit("some error")
+	if reporter.HasNoError() {
+		t.Error("After emit some error, HasNoError should return false but true")
+	}
+}
