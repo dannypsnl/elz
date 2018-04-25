4543c60e1f3a656e3e8cf9aa899e9fd70b651814
dannypsnl <dannypsnl@gmail.com>
2018-04-25 10:49:21 +0800

    test: i32, f32 -> Check, Type

--- ast/primitive_test.go
+++ ast/primitive_test.go
@@ -7,8 +7,9 @@ import (
 func TestI32(t *testing.T) {
 	context := NewContext()
 	iVal := &I32{Val: "10"}
+	iVal.Check(context)
 	llvmIR := iVal.Codegen(context)
-	if llvmIR.Type() != LLVMType("i32") {
+	if llvmIR.Type() != LLVMType(iVal.Type(context)) {
 		t.Error("Bug in ast.I32")
 	}
 }
@@ -16,8 +17,9 @@ func TestI32(t *testing.T) {
 func TestF32(t *testing.T) {
 	context := NewContext()
 	fVal := &F32{Val: "3.14"}
+	fVal.Check(context)
 	llvmIR := fVal.Codegen(context)
-	if llvmIR.Type() != LLVMType("f32") {
+	if llvmIR.Type() != LLVMType(fVal.Type(context)) {
 		t.Error("Bug in ast.F32")
 	}
 }
