2af65f243550e816946505c3155a6a9bef53ef66
dannypsnl <dannypsnl@gmail.com>
2018-04-25 10:54:01 +0800

    test: str

--- ast/primitive_test.go
+++ ast/primitive_test.go
@@ -4,6 +4,20 @@ import (
 	"testing"
 )
 
+func TestStr(t *testing.T) {
+	context := NewContext()
+	str := &Str{Val: "Hello"}
+	str.Check(context)
+	ir := str.Codegen(context)
+	if str.Type(context) != "str" {
+		t.Error("str::Type return type is wrong")
+	}
+	if ir.Type().String() != "ArrayType(IntegerType(8 bits)[5])" {
+		println(ir.Type().String())
+		t.Error("string ir type is incorrect")
+	}
+}
+
 func TestI32(t *testing.T) {
 	context := NewContext()
 	iVal := &I32{Val: "10"}
