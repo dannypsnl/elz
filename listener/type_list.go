beb7f64863d8817fe99e73a8f917d1245413f6d9
dannypsnl <dannypsnl@gmail.com>
2018-04-30 17:53:44 +0800

    add: found a problem at ref with function builder, we will push the type in type

--- listener/type_list.go
+++ listener/type_list.go
@@ -8,6 +8,7 @@ func (s *ElzListener) EnterTypeList(c *parser.TypeListContext) {
 	if s.fnBuilder == nil {
 		panic("Compiler bug, declaration expect have function builder when parsing")
 	}
+	// FIXME: I cause the type in another type also be found
 	for _, typ := range c.AllTypeForm() {
 		s.fnBuilder.PushParamName(".wont_use")
 		s.fnBuilder.PushParamType(typ.GetText())
