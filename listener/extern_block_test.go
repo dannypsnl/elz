1515cc64e2d31a109864c00062f3da27c0b3db09
dannypsnl <dannypsnl@gmail.com>
2018-04-30 18:49:15 +0800

    test: several type in declaration

--- listener/extern_block_test.go
+++ listener/extern_block_test.go
@@ -8,6 +8,8 @@ func TestExternDeclare(t *testing.T) {
 	res := NewParse(`
 	extern "C" {
 		fn add_one(i32) -> i32
+		fn printf(ref<i8>) -> i32
+		fn printf(ref<i8>, i32, i32) -> i32
 	}
 	`)
 
@@ -15,6 +17,10 @@ func TestExternDeclare(t *testing.T) {
 source_filename = "main"
 
 declare i32 @add_one(i32)
+
+declare i32 @printf(i8*)
+
+declare i32 @printf.1(i8*, i32, i32)
 `
 
 	if res != expected {
