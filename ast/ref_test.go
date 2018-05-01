858117bc475493d8fc673dbd541015c14a96bf12
dannypsnl <dannypsnl@gmail.com>
2018-05-01 13:18:23 +0800

    test: ref ast

---
+++ ast/ref_test.go
@@ -0,0 +1,55 @@
+package ast
+
+import (
+	"testing"
+
+	"llvm.org/llvm/bindings/go/llvm"
+)
+
+func TestRefI32(t *testing.T) {
+	c := NewContext()
+
+	ft := llvm.FunctionType(llvm.Int32Type(), []llvm.Type{}, false)
+	main := llvm.AddFunction(c.Module, "main", ft)
+
+	entry := llvm.AddBasicBlock(main, "entry")
+
+	c.Builder.SetInsertPointAtEnd(entry)
+
+	varA := &LocalVarDef{
+		Immutable:  true,
+		Name:       "a",
+		VarType:    "i32",
+		Expression: &I32{Val: "10"},
+	}
+	varB := &LocalVarDef{
+		Immutable:  true,
+		Name:       "b",
+		VarType:    "ref<i32>",
+		Expression: &Ref{E: &Id{Val: "a"}},
+	}
+
+	varA.Check(c)
+	varB.Check(c)
+
+	varA.Codegen(c)
+	varB.Codegen(c)
+
+	actual := c.Module.String()
+
+	expected := `; ModuleID = 'main'
+source_filename = "main"
+
+define i32 @main() {
+entry:
+  %a = alloca i32
+  store i32 10, i32* %a
+  %a1 = load i32, i32* %a
+  %b = alloca i32*
+  store i32* %a, i32** %b
+  %b2 = load i32*, i32** %b
+}
+`
+
+	assertEq(t, actual, expected)
+}
