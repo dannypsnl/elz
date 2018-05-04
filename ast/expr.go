45e92ab43573150809b93b9533652a7b391ebcc3
dannypsnl <dannypsnl@gmail.com>
2018-05-05 02:54:40 +0800

    refactor: move FnCall to fn_call.go

--- ast/expr.go
+++ ast/expr.go
@@ -1,7 +1,6 @@
 package ast
 
 import (
-	"bytes"
 	"fmt"
 
 	"llvm.org/llvm/bindings/go/llvm"
@@ -97,45 +96,4 @@ func (b *BinaryExpr) Type(ctx *Context) string {
 		return "type error"
 	}
 	return leftT
-}
-
-type FnCall struct {
-	Name    string
-	Args    []Expr
-	fcache  string
-	retType string // Setting by parser
-}
-
-func (fc *FnCall) Check(c *Context) {
-	for _, at := range fc.Args {
-		at.Check(c)
-	}
-
-	buf := bytes.NewBuffer([]byte{})
-	buf.WriteString(fc.Name)
-	buf.WriteRune('(')
-	for i, at := range fc.Args {
-		buf.WriteString(at.Type(c))
-		if i < len(fc.Args)-1 {
-			buf.WriteRune(',')
-		}
-	}
-	buf.WriteRune(')')
-	fc.fcache = buf.String()
-	fc.retType = c.funcRetTyp(fc.fcache).retType
-}
-
-func (fc *FnCall) Codegen(c *Context) llvm.Value {
-	fn := c.funcRetTyp(fc.fcache).value
-
-	args := []llvm.Value{}
-	for _, a := range fc.Args {
-		args = append(args, a.Codegen(c))
-	}
-
-	return c.Builder.CreateCall(fn, args, ".call_tmp")
-}
-
-func (fc *FnCall) Type(*Context) string {
-	return fc.retType
 }
---
+++ ast/fn_call.go
@@ -0,0 +1,48 @@
+package ast
+
+import (
+	"bytes"
+
+	"llvm.org/llvm/bindings/go/llvm"
+)
+
+type FnCall struct {
+	Name    string
+	Args    []Expr
+	fcache  string
+	retType string // Setting by parser
+}
+
+func (fc *FnCall) Check(c *Context) {
+	for _, at := range fc.Args {
+		at.Check(c)
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+	buf.WriteString(fc.Name)
+	buf.WriteRune('(')
+	for i, at := range fc.Args {
+		buf.WriteString(at.Type(c))
+		if i < len(fc.Args)-1 {
+			buf.WriteRune(',')
+		}
+	}
+	buf.WriteRune(')')
+	fc.fcache = buf.String()
+	fc.retType = c.funcRetTyp(fc.fcache).retType
+}
+
+func (fc *FnCall) Codegen(c *Context) llvm.Value {
+	fn := c.funcRetTyp(fc.fcache).value
+
+	args := []llvm.Value{}
+	for _, a := range fc.Args {
+		args = append(args, a.Codegen(c))
+	}
+
+	return c.Builder.CreateCall(fn, args, ".call_tmp")
+}
+
+func (fc *FnCall) Type(*Context) string {
+	return fc.retType
+}
