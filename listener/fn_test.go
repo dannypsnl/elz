2044e0262b8a0f82ee25e57861215270c5594444
dannypsnl <dannypsnl@gmail.com>
2018-05-10 12:18:17 +0800

    test: clean up, these change purpose is we don't care Module when testing, so using strings.Contains is enough

--- listener/fn_test.go
+++ listener/fn_test.go
@@ -1,6 +1,7 @@
 package listener
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -11,18 +12,16 @@ func TestConstComputingInFunction(t *testing.T) {
 	}
 	`)
 
-	expected := `; ModuleID = 'main'
-source_filename = "main"
-
-define void @foo() {
-entry:
-  %a = alloca i32
-  store i32 4, i32* %a
-  %a1 = load i32, i32* %a
-}
+	expected := `
+		define void @foo() {
+		entry:
+		  %a = alloca i32
+		  store i32 4, i32* %a
+		  %a1 = load i32, i32* %a
+		}
 `
-	if res != expected {
-		t.Errorf("Function Error; expected: `%s`\nactual: `%s`", expected, res)
+	if strings.Contains(res, expected) {
+		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
 	}
 }
 
@@ -31,15 +30,13 @@ func TestMissTypeComplete(t *testing.T) {
 	fn foo(a, b, c: i32) {}
 	`)
 
-	expected := `; ModuleID = 'main'
-source_filename = "main"
-
-define void @foo(i32 %a, i32 %b, i32 %c) {
-entry:
-}
+	expected := `
+		define void @foo(i32 %a, i32 %b, i32 %c) {
+		entry:
+		}
 `
-	if res != expected {
-		t.Errorf("Function Error; expected: `%s`\nactual: `%s`", expected, res)
+	if strings.Contains(res, expected) {
+		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
 	}
 }
 
@@ -49,19 +46,17 @@ func TestOverloadingFunction(t *testing.T) {
 	fn add(l: i32, r: i32) -> i32 {}
 	`)
 
-	expected := `; ModuleID = 'main'
-source_filename = "main"
+	expected := `
+		define float @add(float %l, float %r) {
+		entry:
+		}
 
-define float @add(float %l, float %r) {
-entry:
-}
-
-define i32 @add.1(i32 %l, i32 %r) {
-entry:
-}
+		define i32 @add.1(i32 %l, i32 %r) {
+		entry:
+		}
 `
-	if res != expected {
-		t.Errorf("Function Error; expected: `%s`\nactual: `%s`", expected, res)
+	if strings.Contains(res, expected) {
+		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
 	}
 }
 
@@ -70,16 +65,14 @@ func TestMainFunction(t *testing.T) {
 	fn main() {}
 	`)
 
-	expected := `; ModuleID = 'main'
-source_filename = "main"
-
-define i32 @main() {
-entry:
-  ret i32 0
-}
+	expected := `
+		define i32 @main() {
+		entry:
+		  ret i32 0
+		}
 `
-	if res != expected {
-		t.Errorf("Main Function Error; expected: `%s`\nactual: `%s`", expected, res)
+	if strings.Contains(res, expected) {
+		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
 	}
 }
 
@@ -88,17 +81,15 @@ func TestBasicComputingFunction(t *testing.T) {
 	fn add(lv, rv: i32) -> i32 { return lv + rv }
 	`)
 
-	expected := `; ModuleID = 'main'
-source_filename = "main"
-
-define i32 @add(i32 %lv, i32 %rv) {
-entry:
-  %.add_tmp = add i32 %lv, %rv
-  ret i32 %.add_tmp
-}
+	expected := `
+		define i32 @add(i32 %lv, i32 %rv) {
+		entry:
+		  %.add_tmp = add i32 %lv, %rv
+		  ret i32 %.add_tmp
+		}
 `
 
-	if res != expected {
+	if strings.Contains(res, expected) {
 		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
 	}
 }
@@ -109,19 +100,17 @@ func TestAccessGlobalVarInFunction(t *testing.T) {
 	fn add_one(v: i32) -> i32 { return v + one }
 	`)
 
-	expected := `; ModuleID = 'main'
-source_filename = "main"
+	expected := `
+		@one = global i32 1
 
-@one = global i32 1
-
-define i32 @add_one(i32 %v) {
-entry:
-  %.add_tmp = add i32 %v, 1
-  ret i32 %.add_tmp
-}
+		define i32 @add_one(i32 %v) {
+		entry:
+		  %.add_tmp = add i32 %v, 1
+		  ret i32 %.add_tmp
+		}
 `
 
-	if res != expected {
+	if strings.Contains(res, expected) {
 		t.Errorf("expected: `%s`\nactual: `%s`", expected, res)
 	}
 }
