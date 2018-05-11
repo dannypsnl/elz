70e3199ead2aa06649bde8fb4b75080c0e2abcf7
dannypsnl <dannypsnl@gmail.com>
2018-05-11 10:10:36 +0800

    test: remove duplicate test

--- ast/expr_test.go
+++ ast/expr_test.go
@@ -6,19 +6,6 @@ import (
 	"llvm.org/llvm/bindings/go/llvm"
 )
 
-func TestF32AST(t *testing.T) {
-	context := NewContext()
-	num := &F32{
-		Val: "3.1415926",
-	}
-	result := num.Codegen(context)
-	if !result.IsConstant() ||
-		result.Type().String() != "FloatType" {
-		result.Dump()
-		t.Error(`error`)
-	}
-}
-
 func TestUnaryExprAST(t *testing.T) {
 	context := NewContext()
 	num := &F32{
@@ -55,20 +42,5 @@ func TestBinaryExprAST(t *testing.T) {
 		result != llvm.ConstFloat(llvm.FloatType(), 3.69) {
 		result.Dump()
 		t.Error(`binary expression fail`)
-	}
-}
-
-func TestStringAST(t *testing.T) {
-	context := NewContext()
-	str := &Str{
-		Val: `"a string with 中文"`,
-	}
-	result := str.Codegen(context)
-	if !result.IsConstant() ||
-		// string is [i8] in LLVM IR
-		result.Type().String() != "ArrayType(IntegerType(8 bits)[22])" ||
-		result != llvm.ConstString(`"a string with 中文"`, false) {
-		result.Dump()
-		t.Error(`error`)
 	}
 }
