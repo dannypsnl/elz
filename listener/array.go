ccbd4b82d5ffea39ce74edd17eba477f5644c83e
dannypsnl <dannypsnl@gmail.com>
2018-05-16 16:31:34 +0800

    feat: impl & test array with expression list

--- listener/array.go
+++ listener/array.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 
 	"github.com/elz-lang/elz/ast"
+	"github.com/elz-lang/elz/collection/stack"
 	"github.com/elz-lang/elz/parser"
 )
 
@@ -27,3 +28,19 @@ func (s *ElzListener) ExitArrWithLen(c *parser.ArrWithLenContext) {
 // '[' expr ';' INT ']'             # ArrWithLen
 // '[' exprList ']'                 # ArrWithList
 //for expr := s.exprStack.Pop(); expr != nil; expr = s.exprStack.Pop() {}
+
+func (s *ElzListener) ExitArrWithList(c *parser.ArrWithListContext) {
+	exprs := make([]ast.Expr, 0)
+	tmp := stack.New()
+	for e := s.exprStack.Pop(); e != nil; e = s.exprStack.Pop() {
+		tmp.Push(e)
+	}
+	for e := tmp.Pop(); e != nil; e = tmp.Pop() {
+		exprs = append(exprs, e.(ast.Expr))
+	}
+	s.exprStack.Push(&ast.Array{
+		Elements:    exprs,
+		ElementType: "",
+		Len:         len(exprs),
+	})
+}
