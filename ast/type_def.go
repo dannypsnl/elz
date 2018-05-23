75470a0234ed8858cb6cb6c1e25fd7dd245781c2
dannypsnl <dannypsnl@gmail.com>
2018-05-23 12:25:52 +0800

    add: create struct type in AST, but not add into Context yet

--- ast/type_def.go
+++ ast/type_def.go
@@ -18,5 +18,10 @@ type TypeDef struct {
 func (typ *TypeDef) Check(c *Context) {}
 
 func (typ *TypeDef) Codegen(c *Context) llvm.Value {
+	types := make([]llvm.Type, 0)
+	for _, attr := range typ.Attrs {
+		types = append(types, LLVMType(attr.Type))
+	}
+	llvm.StructType(types, true)
 	return llvm.Value{}
 }
