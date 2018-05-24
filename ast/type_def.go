b01c1c90b66853b8ba38cb74c60cdcafd0aca718
dannypsnl <dannypsnl@gmail.com>
2018-05-24 20:41:10 +0800

    add: type's constructor function, but fail

--- ast/type_def.go
+++ ast/type_def.go
@@ -1,6 +1,8 @@
 package ast
 
 import (
+	"bytes"
+
 	"llvm.org/llvm/bindings/go/llvm"
 )
 
@@ -28,5 +30,41 @@ func (typ *TypeDef) Codegen(c *Context) llvm.Value {
 
 	c.NewType(typ.Name, t)
 
+	ft := llvm.FunctionType(llvm.PointerType(t, 0), types, false)
+	f := llvm.AddFunction(c.Module, typ.Name, ft)
+
+	entry := llvm.AddBasicBlock(f, "entry")
+
+	c.Builder.SetInsertPointAtEnd(entry)
+
+	object := c.Builder.CreateMalloc(t, "object")
+
+	for i, attr := range typ.Attrs {
+		valueI := c.Builder.CreateGEP(object, []llvm.Value{
+			llvm.ConstInt(llvm.Int32Type(), 0, false),
+			llvm.ConstInt(llvm.Int32Type(), uint64(i), false),
+		}, attr.Name)
+		c.Builder.CreateStore(f.Param(i), valueI)
+	}
+
+	c.Builder.CreateRet(object)
+
+	c.Builder.ClearInsertionPoint()
+
+	signature := bytes.NewBuffer([]byte{})
+	signature.WriteString("(")
+	for i, attr := range typ.Attrs {
+		if i != 0 {
+			signature.WriteString("," + attr.Type)
+		} else {
+			signature.WriteString(attr.Type)
+		}
+	}
+	signature.WriteString(")")
+
+	c.functions[signature.String()] = &Function{
+		value:   f,
+		retType: typ.Name,
+	}
 	return llvm.Value{}
 }
