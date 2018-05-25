1da5f4590468c8ef3d09d28c8824a6dcede300f4
dannypsnl <dannypsnl@gmail.com>
2018-05-25 22:09:36 +0800

    add: some checking to sure we are get element from array, resolve #71

--- ast/array_element.go
+++ ast/array_element.go
@@ -7,17 +7,27 @@ import (
 type ArrayElement struct {
 	E     Expr
 	Index int
+
+	ok bool
 }
 
 func (ae *ArrayElement) Check(c *Context) {
 	ae.E.Check(c)
+
+	ae.ok = isArrayType(ae.E.Type(c))
 }
 
 func (ae *ArrayElement) Codegen(c *Context) llvm.Value {
-	expr := ae.E.Codegen(c)
-	return c.Builder.CreateExtractValue(expr, ae.Index, "")
+	if ae.ok {
+		expr := ae.E.Codegen(c)
+		return c.Builder.CreateExtractValue(expr, ae.Index, "")
+	}
+	return llvm.Value{}
 }
 
 func (ae *ArrayElement) Type(c *Context) string {
-	return elemType(ae.E.Type(c))
+	if ae.ok {
+		return elemType(ae.E.Type(c))
+	}
+	return ""
 }
