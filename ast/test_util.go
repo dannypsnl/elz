7206f77112c41eb6cbdee443a68f016143be6de5
dannypsnl <dannypsnl@gmail.com>
2018-06-01 21:39:56 +0800

    test: TypeDef AST correct result

--- ast/test_util.go
+++ ast/test_util.go
@@ -1,11 +1,18 @@
 package ast
 
 import (
+	"strings"
 	"testing"
 )
 
 func assertEq(t *testing.T, actual, expected interface{}) {
 	if actual != expected {
 		t.Errorf("expected: `%v`, actual: `%v`", expected, actual)
+	}
+}
+
+func testHas(t *testing.T, res, expectedIn string) {
+	if !strings.Contains(res, expectedIn) {
+		t.Errorf("expected has: `%s`, actual: `%s`", expectedIn, res)
 	}
 }
