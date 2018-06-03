67e219155830ef22966bb1291168031b50db1cb2
dannypsnl <dannypsnl@gmail.com>
2018-06-03 23:18:39 +0800

    refactor: add method for type define builder to make it clear

--- listener/type_def.go
+++ listener/type_def.go
@@ -5,9 +5,45 @@ import (
 	"github.com/elz-lang/elz/parser"
 )
 
+// EnterTypeDefine create a type define builder for following recording task
+func (s *ElzListener) EnterTypeDefine(c *parser.TypeDefineContext) {
+	s.typeDefineBuilder = new(TypeDefineBuilder)
+}
+
+// ExitAttr insert (export, field name, field type) infos into type define builder
+func (s *ElzListener) ExitAttr(c *parser.AttrContext) {
+	if s.typeDefineBuilder == nil {
+		panic("compiler bug, type define builder must be not nil in rule attr")
+	}
+	s.typeDefineBuilder.newAttr(
+		s.exportThis,
+		c.ID().GetText(),
+		c.TypeForm().GetText(),
+	)
+}
+
+func (s *ElzListener) ExitTypeDefine(c *parser.TypeDefineContext) {
+	if s.typeDefineBuilder == nil {
+		panic("compiler bug, type define builder must be not nil in rule type define")
+	}
+	s.typeDefineBuilder.typeName = c.ID().GetText()
+	s.AstList = append(s.AstList, s.typeDefineBuilder.generate())
+
+	// clean up type define builder  after use
+	s.typeDefineBuilder = nil
+}
+
 type TypeDefineBuilder struct {
 	attrs    []ast.TypeAttr
 	typeName string
+}
+
+func (t *TypeDefineBuilder) newAttr(exportOrNot bool, fieldName, fieldTyp string) {
+	t.attrs = append(t.attrs, ast.TypeAttr{
+		Export: exportOrNot,
+		Name:   fieldName,
+		Typ:    fieldTyp,
+	})
 }
 
 func (t *TypeDefineBuilder) generate() *ast.TypeDef {
@@ -15,33 +51,4 @@ func (t *TypeDefineBuilder) generate() *ast.TypeDef {
 		Name:  t.typeName,
 		Attrs: t.attrs,
 	}
-}
-
-func (s *ElzListener) EnterTypeDefine(c *parser.TypeDefineContext) {
-	s.typeDefineBuilder = new(TypeDefineBuilder)
-}
-
-func (s *ElzListener) ExitAttr(c *parser.AttrContext) {
-	if s.typeDefineBuilder == nil {
-		panic("compiler bug, type define builder must be not nil in rule attr")
-	}
-	attr := ast.TypeAttr{
-		Export: s.exportThis,
-		Name:   c.ID().GetText(),
-		Typ:    c.TypeForm().GetText(),
-	}
-	s.typeDefineBuilder.attrs = append(s.typeDefineBuilder.attrs, attr)
-}
-
-func (s *ElzListener) ExitTypeDefine(c *parser.TypeDefineContext) {
-	if s.typeDefineBuilder == nil {
-		panic("compiler bug, type define builder must be not nil in rule type define")
-	}
-
-	s.typeDefineBuilder.typeName = c.ID().GetText()
-
-	s.AstList = append(s.AstList, s.typeDefineBuilder.generate())
-
-	// clean up after use
-	s.typeDefineBuilder = nil
 }
