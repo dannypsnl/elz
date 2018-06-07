89bf21c2260cf77e4b0d9fb7ca67639e2d82c87b
dannypsnl <dannypsnl@gmail.com>
2018-06-07 19:07:02 +0800

    add: MatchBuilder & helper type

--- listener/match_rule.go
+++ listener/match_rule.go
@@ -1,8 +1,26 @@
 package listener
 
 import (
+	"github.com/elz-lang/elz/ast"
 	"github.com/elz-lang/elz/parser"
 )
+
+// Pattern represents:
+//
+//  match E {
+//    // Pattern
+//    expression => statement
+//  }
+type Pattern struct {
+	expr ast.Expr
+	stat ast.Stat
+}
+
+type MatchBuilder struct {
+	// match expr {}
+	expr     ast.Expr
+	patterns []Pattern
+}
 
 func (s *ElzListener) EnterMatchRule(c *parser.MatchRuleContext) {
 	println("match " + c.MatchExpr().GetText())
