bbde7b9eb92b5e06392364deb058b612bcef7ff2
dannypsnl <dannypsnl@gmail.com>
2018-06-10 15:27:05 +0800

    refactor: variable listen method, fix export consuming place

--- listener/var_def.go
+++ listener/var_def.go
@@ -29,37 +29,40 @@ func (s *ElzListener) ExitGlobalVarDef(*parser.GlobalVarDefContext) {
 // Def:
 //   $var_name = $expr
 func (s *ElzListener) ExitDefine(ctx *parser.DefineContext) {
-	// get expr
-	expr := s.exprStack.Pop()
-	// get type from expression
-	var typ string
 	// get identifier
 	// TODO: fix with scope rule, and some rule to detected fn, type, trait or what
 	name := ctx.ID().GetText()
+	// get type from expression
+	var typ string
 	// get type from source code, so we can find out the problem if expr != user_def type
 	if ctx.TypeForm() != nil {
 		typ = ctx.TypeForm().GetText()
 	}
 
-	if s.exportThis {
-		s.exportThis = false
-	}
+	// get expr
+	expr := s.exprStack.Pop().(ast.Expr)
 
 	// FIXME: Need to classify heap & stack, and can find out the lifetime, else return something in stack will become bug
+	// TODO: match rule although cause consume statement, have to preparing this
 	if s.isGlobalDef {
 		s.AstList = append(s.AstList, &ast.GlobalVarDef{
 			Export:     s.exportThis,
 			Name:       name,
 			VarType:    typ,
-			Expression: expr.(ast.Expr),
+			Expression: expr,
 		})
+		// consume export effect
+		if s.exportThis {
+			s.exportThis = false
+		}
 		// Record type for compiler
 	} else if s.fnBuilder != nil {
+		// local variable won't be exported
 		s.fnBuilder.Stat(&ast.LocalVarDef{
 			Immutable:  s.immutable,
 			Name:       name,
 			VarType:    typ,
-			Expression: expr.(ast.Expr),
+			Expression: expr,
 		})
 	} else {
 		panic("A define should be Global Var else is Local Var, but both of this isn't")
