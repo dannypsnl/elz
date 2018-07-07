c6f36c008d6b4ef42bd5acd5e129e991cc0ba1c4
dannypsnl <dannypsnl@gmail.com>
2018-07-07 12:06:34 +0800

    test: match rule AST
    note: should test others AST with match rule by LLVM IR could different
    at different version and is not stable & not robust, testing the code
    behavior is much more better

---
+++ ast/match_rule_test.go
@@ -0,0 +1,46 @@
+package ast
+
+import (
+	"testing"
+	"github.com/dannypsnl/assert"
+
+	"github.com/elz-lang/elz/util"
+
+	"llvm.org/llvm/bindings/go/llvm"
+)
+
+func Test_match_rule(t *testing.T) {
+	assert := assert.NewTester(t)
+	c := NewContext()
+
+	p1 := Pattern{
+		E: &I32{Val: "10"},
+		S: &Return{
+			&I32{Val: "1"},
+		},
+	}
+	m := NewMatch(&I32{Val: "10"}, []*Pattern{&p1})
+	nextStat := &Return{
+		&I32{Val: "0"},
+	}
+
+	f := &FnDef{
+		Export:    false,
+		Name:      "foo",
+		Params:    []*Param{},
+		Body:      []Stat{m, nextStat},
+		RetType:   "i32",
+		Ctx:       c,
+		Notations: []util.Notation{},
+	}
+
+	f.Check(c)
+	f.Codegen(c)
+
+	testEngine, err := llvm.NewExecutionEngine(c.Module)
+	if err != nil {
+		panic(err)
+	}
+	result := testEngine.RunFunction(f.Codegen(c), []llvm.GenericValue{})
+	assert.Eq(result.Int(true), uint64(1))
+}
