06bd1d84f6c28e707548d0f456a99931742d77e5
dannypsnl <dannypsnl@gmail.com>
2018-07-21 21:59:07 +0800

    add: basic structure for #68
    - mod test.elz for following experimental
    - create statement AST: `Loop`, `BreakStat`
    - push break statement into list of statement when exit break statement

--- ast/loop.go
+++ ast/loop.go
@@ -1,1 +1,18 @@
 package ast
+
+import (
+	"llvm.org/llvm/bindings/go/llvm"
+)
+
+type Loop struct {
+
+}
+
+type BreakStat struct {
+
+}
+
+func (b *BreakStat) Check(*Context) {}
+func (b *BreakStat) Codegen(c *Context) llvm.Value {
+	return llvm.Value{}
+}
--- listener/loop.go
+++ listener/loop.go
@@ -1,1 +1,10 @@
 package listener
+
+import (
+	"github.com/elz-lang/elz/parser"
+	"github.com/elz-lang/elz/ast"
+)
+
+func (s *ElzListener) ExitBreakStat(c *parser.BreakStatContext) {
+	s.stats = append(s.stats, &ast.BreakStat{})
+}
