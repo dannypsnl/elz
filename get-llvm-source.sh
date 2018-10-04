6bd6d4d25652789e270d0566853cff0aa1ecd3af
dannypsnl <dannypsnl@gmail.com>
2018-10-04 22:06:21 +0800

    test: give package a version control tool

--- get-llvm-source.sh
+++ get-llvm-source.sh
@@ -5,6 +5,7 @@ case "${LLVM_VER}" in
         curl -XGET http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz >> llvm.tar.xz
         tar -xf llvm.tar.xz
         mv llvm-6.0.0.src llvm
+        cd llvm && git init
         ;;
     *)
         go get -d llvm.org/llvm/bindings/go/llvm
