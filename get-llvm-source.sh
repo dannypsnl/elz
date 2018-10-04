c3f078569dd52ef1cc694cbb19ace19b842d9057
dannypsnl <dannypsnl@gmail.com>
2018-10-04 22:13:46 +0800

    test: add origin

--- get-llvm-source.sh
+++ get-llvm-source.sh
@@ -5,7 +5,7 @@ case "${LLVM_VER}" in
         curl -XGET http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz >> llvm.tar.xz
         tar -xf llvm.tar.xz
         mv llvm-6.0.0.src llvm
-        cd llvm && git init
+        cd llvm && git init && git remote add origin https://github.com/llvm-mirror/llvm.git
         ;;
     *)
         go get -d llvm.org/llvm/bindings/go/llvm
