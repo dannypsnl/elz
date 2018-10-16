3b93f522347fc80cee54676c4a5fdc5367827201
林子篆 <dannypsnl@gmail.com>
2018-10-16 17:13:24 +0800

    Use visitor for code generation (#99)
    * add: visitor structure
    
    * add: try some inkwell
    
    * test: give llvm prefix
    
    * test: missing -prefix argument by wrong syntax
    
    * test: --prefix, not -prefix
    
    * add: detect Number ast can convert to what kind of number to generate
    global bind
    
    FIXME: global bind should use exported this part
    
    * test: should be Integer, not Number ast, fix outdated test
    
    * add: return module from visitor, then create bytecode file by it
    
    * add: add functon into module
    
    TODO: create statements for function
    
    * fix: function return type parsing
    
    * refactor: reduce fn_type generation
    
    * add: set parameter value name
    
    * style: use `use` reduce code

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use clap::{App, Arg, SubCommand};
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
+extern crate inkwell;
+
+use std::fs::File;
 
 mod ast;
 mod parser;
+mod visit;
 
 fn main() {
     let matches = App::new("elz")
@@ -28,6 +32,10 @@ fn main() {
         let file_name = matches.value_of("INPUT").unwrap(); // missing at is fine, just panic.
 
         // FIXME: return AST tree here
-        parser::parse_elz_program(file_name);
+        let ast_tree = parser::parse_elz_program(file_name);
+        let mut visitor = visit::Visitor::new();
+        let module = visitor.visit_program(ast_tree);
+        let mut output = File::create("test.bc").unwrap();
+        module.write_bitcode_to_file(&output, true, true);
     }
 }
