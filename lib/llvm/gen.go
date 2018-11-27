e5b68b588de32775cee0276842b0d49f3da8c3f7
dannypsnl <dannypsnl@gmail.com>
2018-11-28 01:47:17 +0800

    fix: add missing LDFLAG -lffi

--- lib/llvm/gen.go
+++ lib/llvm/gen.go
@@ -1,3 +1,6 @@
 package llvm
 
 //go:generate sh -c "go build ./tool/llvm-go.go && ./llvm-go print-config > ./llvm_config.go && rm llvm-go"
+
+// #cgo LDFLAGS: -L/usr/local/Cellar/llvm/7.0.0_1/lib -Wl,-search_paths_first -Wl,-headerpad_max_install_names -lffi
+import "C"
