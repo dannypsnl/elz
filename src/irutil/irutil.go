e54098b9b86fcaa6eefc7f38043dc20de9894653
dannypsnl <dannypsnl@gmail.com>
2019-05-11 14:01:31 +0800

    fix irutil, while fixing the number duplicate, can't use num.num format

--- src/irutil/irutil.go
+++ src/irutil/irutil.go
@@ -2,6 +2,7 @@ package irutil
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/llir/llvm/ir"
 )
@@ -33,9 +34,24 @@ func fixName(counter map[string]uint64, identifier identifier) {
 	originName := identifier.Name()
 	curCnt := counter[originName]
 	if curCnt > 0 {
+		// if string is a int as 0, 1
+		if v, err := strconv.Atoi(originName); err == nil {
+			fixNumberName(v, counter, identifier)
+			return
+		}
 		newName := fmt.Sprintf("%s.%d", originName, curCnt)
 		identifier.SetName(newName)
 		counter[newName]++
 	}
 	counter[originName]++
+}
+
+func fixNumberName(v int, counter map[string]uint64, identifier identifier) {
+	newName := fmt.Sprintf("%d", v+1)
+	_, ok := counter[newName]
+	if ok {
+		fixNumberName(v+1, counter, identifier)
+	}
+	identifier.SetName(newName)
+	counter[newName]++
 }
