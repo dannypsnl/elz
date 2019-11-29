3714d203fd59cf1b5af029418c08eb74e0ddd246
dannypsnl <dannypsnl@gmail.com>
2019-11-29 20:41:10 +0800

    add header protect

--- lib-runtime/prelude/string.hpp
+++ lib-runtime/prelude/string.hpp
@@ -1,7 +1,16 @@
+//
+// Created by dannypsnl on 2019/11/29.
+//
+
+#ifndef ELZ_LIB_RUNTIME_PRELUDE_STRING_H_
+#define ELZ_LIB_RUNTIME_PRELUDE_STRING_H_
+
 #include <string>
 
 class String {
-public:
+ public:
   std::string value;
   String(char *lit) : value(lit) {}
 };
+
+#endif //ELZ_LIB_RUNTIME_PRELUDE_STRING_H_
