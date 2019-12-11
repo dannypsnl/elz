9f0b2c8e56556036e2daa40ebbfd0540936a2847
dannypsnl <dannypsnl@gmail.com>
2019-12-11 16:19:20 +0800

    refactor: improve semantic error code style

--- src/semantic/error.rs
+++ src/semantic/error.rs
@@ -5,49 +5,74 @@ use thiserror::Error;
 pub type Result<T> = std::result::Result<T, SemanticError>;
 
 #[derive(Debug, Error)]
-pub enum SemanticError {
-    #[error("{} name: `{}` be redefined", .0, .1)]
-    NameRedefined(Location, String),
-    #[error("{} type mismatched, expected: `{}` but got: `{}`", .0, .1, .2)]
-    TypeMismatched(Location, Type, Type),
-    #[error("{} no variable named: `{}`", .0, .1)]
-    NoVariableNamed(Location, String),
-    #[error("{} no type named: `{}`", .0,  .1)]
-    NoTypeNamed(Location, String),
-    #[error("{} call on non-function type: `{}`", .0, .1)]
-    CallOnNonFunctionType(Location, Type),
-    #[error("{} following fields must be inited but haven't: {}", .0, ShowFieldsList(.1.to_vec()))]
-    FieldsMissingInit(Location, Vec<String>),
-    #[error("{} cannot use class construction on a non-class type: {}", .0, .1)]
-    CannotConstructNonClassType(Location, Type),
-    #[error("{} cannot use class construction out of class scope", .0)]
-    CannotUseClassConstructionOutOfClass(Location),
+#[error("{location} {err}")]
+pub struct SemanticError {
+    location: Location,
+    err: SemanticErrorVariant,
+}
+
+#[derive(Debug, Error)]
+enum SemanticErrorVariant {
+    #[error("name: `{}` be redefined", .0)]
+    NameRedefined(String),
+    #[error("type mismatched, expected: `{}` but got: `{}`", .0, .1)]
+    TypeMismatched(Type, Type),
+    #[error("no variable named: `{}`", .0)]
+    NoVariableNamed(String),
+    #[error("no type named: `{}`",  .0)]
+    NoTypeNamed(String),
+    #[error("call on non-function type: `{}`", .0)]
+    CallOnNonFunctionType(Type),
+    #[error("following fields must be inited but haven't: {}", ShowFieldsList(.0.to_vec()))]
+    FieldsMissingInit(Vec<String>),
+    #[error("cannot use class construction on a non-class type: {}", .0)]
+    CannotConstructNonClassType(Type),
+    #[error("cannot use class construction out of class scope")]
+    CannotUseClassConstructionOutOfClass(),
 }
 
 impl SemanticError {
+    fn new(location: &Location, err: SemanticErrorVariant) -> SemanticError {
+        SemanticError {
+            location: location.clone(),
+            err,
+        }
+    }
     pub fn name_redefined<T: ToString>(location: &Location, name: T) -> SemanticError {
-        SemanticError::NameRedefined(location.clone(), name.to_string())
+        SemanticError::new(
+            location,
+            SemanticErrorVariant::NameRedefined(name.to_string()),
+        )
     }
     pub fn type_mismatched(location: &Location, expected: Type, actual: Type) -> SemanticError {
-        SemanticError::TypeMismatched(location.clone(), expected, actual)
+        SemanticError::new(
+            location,
+            SemanticErrorVariant::TypeMismatched(expected, actual),
+        )
     }
     pub fn no_variable(location: &Location, name: String) -> SemanticError {
-        SemanticError::NoVariableNamed(location.clone(), name)
+        SemanticError::new(location, SemanticErrorVariant::NoVariableNamed(name))
     }
     pub fn no_type(location: &Location, name: String) -> SemanticError {
-        SemanticError::NoTypeNamed(location.clone(), name)
+        SemanticError::new(location, SemanticErrorVariant::NoTypeNamed(name))
     }
     pub fn call_on_non_function_type(location: &Location, typ: Type) -> SemanticError {
-        SemanticError::CallOnNonFunctionType(location.clone(), typ)
+        SemanticError::new(location, SemanticErrorVariant::CallOnNonFunctionType(typ))
     }
     pub fn fields_missing_init(location: &Location, fields: Vec<String>) -> SemanticError {
-        SemanticError::FieldsMissingInit(location.clone(), fields)
+        SemanticError::new(location, SemanticErrorVariant::FieldsMissingInit(fields))
     }
     pub fn cannot_construct_non_class_type(location: &Location, typ: Type) -> SemanticError {
-        SemanticError::CannotConstructNonClassType(location.clone(), typ)
+        SemanticError::new(
+            location,
+            SemanticErrorVariant::CannotConstructNonClassType(typ),
+        )
     }
     pub fn cannot_use_class_construction_out_of_class(location: &Location) -> SemanticError {
-        SemanticError::CannotUseClassConstructionOutOfClass(location.clone())
+        SemanticError::new(
+            location,
+            SemanticErrorVariant::CannotUseClassConstructionOutOfClass(),
+        )
     }
 }
 
