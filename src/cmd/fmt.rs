850a4f0f55acc24db05b22285d545f453b059bef
dannypsnl <dannypsnl@gmail.com>
2019-12-30 22:30:51 +0800

    (#205) tool: command `elz fmt ./` complete

--- src/cmd/fmt.rs
+++ src/cmd/fmt.rs
@@ -1,4 +1,6 @@
+use crate::ast::format::{FormatTopAstList, FormattedElz, DEFAULT_LEVEL};
 use crate::parser::Parser;
+use std::io::Write;
 use walkdir::{DirEntry, WalkDir};
 
 pub const CMD_NAME: &'static str = "fmt";
@@ -15,9 +17,11 @@ fn handle_entry(entry: DirEntry) -> Result<(), Box<dyn std::error::Error>> {
     let file_name = entry.file_name().to_string_lossy().to_string();
     if file_name.ends_with(".elz") {
         let code = std::fs::read_to_string(entry.path())?;
-        let _program = Parser::parse_program(entry.path().to_string_lossy().to_string(), code)?;
-        // TODO: implements program format function
-        println!("{}", entry.path().display());
+        let program = Parser::parse_program(entry.path().to_string_lossy().to_string(), code)?;
+        let result = FormatTopAstList(program).formatted_elz(DEFAULT_LEVEL);
+        let mut origin_file = std::fs::File::create(entry.path())?;
+        origin_file.write_all(result.as_bytes())?;
+        origin_file.write_all("\n".as_bytes())?;
     }
     Ok(())
 }
