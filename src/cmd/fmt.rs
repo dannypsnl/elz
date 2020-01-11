0e5b15d02aaf27f0a280b92de28dc6715803ad8c
dannypsnl <dannypsnl@gmail.com>
2020-01-11 14:54:57 +0800

    (#228) tool: code formatter also accept files

--- src/cmd/fmt.rs
+++ src/cmd/fmt.rs
@@ -1,25 +1,42 @@
 use crate::codegen::formatted_elz::{FormatTopAstList, FormattedElz, DEFAULT_LEVEL};
 use crate::parser::Parser;
+use std::ffi::OsStr;
 use std::io::Write;
-use walkdir::{DirEntry, WalkDir};
+use std::path::Path;
+use walkdir::WalkDir;
 
 pub const CMD_NAME: &'static str = "fmt";
 
-pub fn format(directory: Vec<&str>) -> Result<(), Box<dyn std::error::Error>> {
-    let dir = directory[0];
-    for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
-        handle_entry(entry)?;
+pub fn format(paths: Vec<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    for path_str in paths {
+        let path = Path::new(path_str);
+        path.extension().map_or_else(
+            || handle_dir(path),
+            |extension| {
+                if extension == "elz" {
+                    handle_entry(path)
+                } else {
+                    handle_dir(path)
+                }
+            },
+        )?;
     }
     Ok(())
 }
 
-fn handle_entry(entry: DirEntry) -> Result<(), Box<dyn std::error::Error>> {
-    let file_name = entry.file_name().to_string_lossy().to_string();
-    if file_name.ends_with(".elz") {
-        let code = std::fs::read_to_string(entry.path())?;
-        let program = Parser::parse_program(entry.path().to_string_lossy().to_string(), code)?;
+fn handle_dir(dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
+        handle_entry(entry.path())?;
+    }
+    Ok(())
+}
+
+fn handle_entry(file_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    if file_path.extension() == Some(OsStr::new("elz")) {
+        let code = std::fs::read_to_string(file_path)?;
+        let program = Parser::parse_program(file_path.to_string_lossy().to_string(), code)?;
         let result = FormatTopAstList(program).formatted_elz(DEFAULT_LEVEL);
-        let mut origin_file = std::fs::File::create(entry.path())?;
+        let mut origin_file = std::fs::File::create(file_path)?;
         origin_file.write_all(result.as_bytes())?;
         origin_file.write_all("\n".as_bytes())?;
     }
